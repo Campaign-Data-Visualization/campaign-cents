6d17fa8dbbbb3d851478bbf0699900141a336446
Jonathan Bird <jonbird81@gmail.com>
2014-07-31 19:36:32 -0700

    Top Organization section uses angular to load donors from scope

--- client/candidateProfile/candidateProfile.js
+++ client/candidateProfile/candidateProfile.js
@@ -1,5 +1,4 @@
 angular.module('myApp.main.candidateProfile', ['ui.router'])
-
 .config(function ($stateProvider) {
 
   $stateProvider
@@ -10,6 +9,28 @@ angular.module('myApp.main.candidateProfile', ['ui.router'])
     });
 })
 .controller('CandidateProfileController', function ($scope) {
-  $scope.message = 'See the candiadate and related data';
+  $scope.message = 'See the candidate and related data';
   $scope.name = 'Ted Cruz';
+  $scope.funding =  {
+    donors: [
+                    {
+                      'recipient': 'Ted Cruz',
+                      'title': 'National Rifle Association',
+                      'amount': 5000,
+                      'year': 2009
+                    },
+                    {
+                      'recipient': 'Ted Cruz',
+                      'title': 'Republican National Committee',
+                      'amount': 2500,
+                      'year': 2009
+                    },
+                    {
+                      'recipient': 'Ted Cruz',
+                      'title': 'Pizza Hut',
+                      'amount': 3000,
+                      'year': 2010
+                    }
+            ]
+  };
 });
