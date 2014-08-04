66aa8361eb6618559a8e3da57cadd8d4fe00b517
Jonathan Bird <jonbird81@gmail.com>
2014-08-04 12:17:23 -0700

    Loads basic funding info from org controller

--- client/orgProfile/orgProfile.js
+++ client/orgProfile/orgProfile.js
@@ -13,4 +13,6 @@ angular.module('myApp.main.orgProfile', ['ui.router'])
   $scope.message = 'Check out this org profile. So so dirty.';
   $scope.org = 'Friends of Prosperity';
   $scope.profile = "Swag messenger bag tattooed, Banksy ethical dreamcatcher freegan skateboard street art ennui next level DIY beard High Life. Blue Bottle cliche small batch freegan. Banjo pop-up freegan High Life typewriter. Tumblr keffiyeh organic cornhole selfies kitsch lomo, tattooed Thundercats put a bird on it. Master cleanse pop-up direct trade, hashtag you probably haven't heard of them twee butcher before they sold out Truffaut. Try-hard synth freegan tote bag, photo booth ethical +1 Helvetica. PBR&B XOXO deep v cornhole meh freegan."
+  $scope.tierOne = 12000;
+  $scope.tierTwo = 28599;
 });
