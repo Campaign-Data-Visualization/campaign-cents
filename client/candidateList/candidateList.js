9d6d6fbeef3f16982bd9f90a63a092905aec168c
Jonathan Bird <jonbird81@gmail.com>
2014-08-20 11:07:32 -0700

    Accidentally commented out DataRequestFactory issue fixed

--- client/candidateList/candidateList.js
+++ client/candidateList/candidateList.js
@@ -15,9 +15,9 @@ angular.module('myApp.main.candidateList', ['ui.router'])
   $scope.name = "Ted Cruz";
   $scope.input = $stateParams.input;
   $scope.candidates = {'house': [{name: "Marlo Longley", party: "Libertarian/KochParty"},{name: "Liam Dorpalen", party: "Democrat"}], 'senate': [{name: "Liam Dorpalen", party: "Democrat"},{name: "Liam Dorpalen", party: "Democrat"}]};
-  // DataRequestFactory.getData($stateParams.input, function(arrayOfCandidates){
+  DataRequestFactory.getData($stateParams.input, function(arrayOfCandidates){
   
-  // })
+  })
   
 });
 
