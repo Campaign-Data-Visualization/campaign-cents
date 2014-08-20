de3849c1732b2e7c67f9270fca77050aea24346e
Jonathan Bird <jonbird81@gmail.com>
2014-08-20 12:28:37 -0700

    Revert to 3ecd6f6

--- client/candidateList/candidateList.js
+++ client/candidateList/candidateList.js
@@ -14,9 +14,10 @@ angular.module('myApp.main.candidateList', ['ui.router'])
   // filter candidates by zipcode input from search page
   $scope.name = "Ted Cruz";
   $scope.input = $stateParams.input;
-  $scope.candidates = {'house': [{name: "Marlo Longley", party: "Libertarian/KochParty"},{name: "Liam Dorpalen", party: "Democrat"}], 'senate': [{name: "Liam Dorpalen", party: "Democrat"},{name: "Liam Dorpalen", party: "Democrat"}]};
+  // $scope.candidates = {'house': [{name: "Marlo Longley", party: "Libertarian/KochParty"},{name: "Liam Dorpalen", party: "Democrat"}], 'senate': [{name: "Liam Dorpalen", party: "Democrat"},{name: "Liam Dorpalen", party: "Democrat"}]};
   DataRequestFactory.getData($stateParams.input, function(arrayOfCandidates){
-  
+    $scope.candidates = arrayOfCandidates;
+
   })
   
 });
