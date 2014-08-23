eb58713880284ebd0c8926701d720e590b4fd20b
hackystack <dorpalenbarry@gmail.com>
2014-08-22 21:06:31 -0700

    working through search directive. Have two working directives on the DOM. Next step is to refactor code inside angucomplete.js to reflect the change

--- client/landingPage/autocomplete/angucomplete.js
+++ client/landingPage/autocomplete/angucomplete.js
@@ -6,6 +6,7 @@
 
 angular.module('angucomplete', [] )
     .directive('angucomplete', function ($parse, $http, $sce, $timeout) {
+        
     return {
         restrict: 'EA',
         scope: {
@@ -23,7 +24,8 @@ angular.module('angucomplete', [] )
             "localData": "=localdata",
             "searchFields": "@searchfields",
             "minLengthUser": "@minlength",
-            "matchClass": "@matchclass"
+            "matchClass": "@matchclass",
+            "zipsearchshow": "=zipsearchshow"
         },
         template: '<div class="angucomplete-holder"><input id="{{id}}_value" ng-model="searchStr" type="text" placeholder="{{placeholder}}" class="{{inputClass}}" onmouseup="this.select();" ng-focus="resetHideResults()" ng-blur="hideResults()" /><div id="{{id}}_dropdown" class="angucomplete-dropdown" ng-if="showDropdown"><div class="angucomplete-searching" ng-show="searching">Searching...</div><div class="angucomplete-searching" ng-show="!searching && (!results || results.length == 0)">No results found</div><div class="angucomplete-row" ng-repeat="result in results| limitTo:5" ng-click="selectResult(result)" ng-mouseover="hoverRow()" ng-class="{\'angucomplete-selected-row\': $index == currentIndex}"><div ng-if="imageField" class="angucomplete-image-holder"><img ng-if="result.image && result.image != \'\'" ng-src="{{result.image}}" class="angucomplete-image"/><div ng-if="!result.image && result.image != \'\'" class="angucomplete-image-default"></div></div><div class="angucomplete-title" ng-if="matchClass" ng-bind-html="result.title"></div><div class="angucomplete-title" ng-if="!matchClass">{{ result.title }}</div><div ng-if="result.description && result.description != \'\'" class="angucomplete-description">{{result.description}}</div></div></div></div>',
 
@@ -51,6 +53,7 @@ angular.module('angucomplete', [] )
             }
 
             $scope.processResults = function(responseData, str) {
+                console.log($scope.searchStr, ' searchStr')
                 if (responseData && responseData.length > 0) {
                     $scope.results = [];
 
@@ -159,6 +162,22 @@ angular.module('angucomplete', [] )
             }
 
             $scope.keyPressed = function(event) {
+                console.log(event.which)
+                if(!isNaN($scope.searchStr)){
+                  $scope.zipsearchshow = true;
+                }else{
+                  $scope.zipsearchshow = false
+                }
+
+                console.log($scope.zipsearchshow);
+
+                // if(event.which <= 57 && event.which >= 49 ){
+                //   $scope.zipsearchshow = true;
+                // }else{
+                //   $scope.zipsearchshow = false
+                // }
+
+
                 if (!(event.which == 38 || event.which == 40 || event.which == 13)) {
                     if (!$scope.searchStr || $scope.searchStr == "") {
                         $scope.showDropdown = false;
