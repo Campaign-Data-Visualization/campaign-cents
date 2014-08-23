612f2776e70e6ba4b2d782c1fb0e280a5047c46e
Jonathan Bird <jonbird81@gmail.com>
2014-08-23 01:41:42 -0700

    Data flows through candidate profile

--- client/candidateProfile/candidateProfile.js
+++ client/candidateProfile/candidateProfile.js
@@ -4,12 +4,18 @@ angular.module('myApp.main.candidateProfile', ['ui.router'])
 
   $stateProvider
     .state('myApp.main.candidateProfile', {
+<<<<<<< HEAD
       url: '/name:{input}/id:{id}',
+=======
+      // url: '/search:{input}',
+      url: '/candidateProfile:{input}',
+>>>>>>> Data flows through candidate profile
       templateUrl: 'candidateProfile/candidateProfile.tpl.html',
       controller: 'CandidateProfileController'
     });
 })
-.controller('CandidateProfileController', function ($scope, $http) {
+.controller('CandidateProfileController', function($scope, $stateParams, DataRequestFactory) {
+    $scope.input = $stateParams.input;
   // $scope.id = "26732";
  //  $scope.loadCandidateBio = function(){
  //    $http.get("" + $scope.id + "&o=JSON" 
@@ -20,4 +26,13 @@ angular.module('myApp.main.candidateProfile', ['ui.router'])
  //       console.log('An unexpected error ocurred!');
  //   });
  // };
+  DataRequestFactory.getData($stateParams.input, function(response){
+    console.log("This is the response in the factory", response);
+    $scope.candidateBio = response;
+    console.log("In the factory should show candidateBio object", $scope.candidateBio); 
+  });
+
+
+
+
 });
