05869aab3e2c68f14f1ce12a3d00397d9a40394e
Greg Michalec <greg@primate.net>
2014-08-30 15:45:44 -0700

    removed some cruft rearranged processing tools into 'backend' folder checking in sql dumps for leadership_pacs and koch_orgs (unreproducable data)

---
+++ backend/sql/koch_orgs.sql
@@ -0,0 +1,51 @@
+-- MySQL dump 10.13  Distrib 5.5.38, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: kochtracker
+-- ------------------------------------------------------
+-- Server version	5.5.38-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `koch_orgs`
+--
+
+DROP TABLE IF EXISTS `koch_orgs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `koch_orgs` (
+  `org_name` varchar(100) NOT NULL,
+  `tier` int(11) NOT NULL,
+  PRIMARY KEY (`org_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `koch_orgs`
+--
+
+LOCK TABLES `koch_orgs` WRITE;
+/*!40000 ALTER TABLE `koch_orgs` DISABLE KEYS */;
+INSERT INTO `koch_orgs` VALUES ('60 Plus Assn',1),('American Commitment',1),('American Future Fund',1),('Americans for Tax Reform',1),('Club for Growth',1),('Freedom Works PAC',0),('Heritage Action for America',1),('Heritage Foundation',1),('Koch Fertilizer',0),('Koch Industries',1),('McKinley Inc',0),('National Taxpayers Union',1),('Republican Jewish Coalition',2),('Right to Life',2),('Wesley Medical Center',0),('Young Americans for Liberty',1);
+/*!40000 ALTER TABLE `koch_orgs` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-08-30 15:44:16
