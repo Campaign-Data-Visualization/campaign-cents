2d42c949a25e87a42829e90f815da1b20abdc325
Greg Michalec <greg@primate.net>
2014-10-07 16:43:41 -0700

    cache candidate profiles unify cache update code

--- server/admin/admin_controllers.js
+++ server/admin/admin_controllers.js
@@ -92,11 +92,9 @@ module.exports = exports = {
                       cleanupDeferred.resolve();
                     }, cleanupDeferred.reject);
                   } else if (sheet == 'realtime') {
+                    var update = require("./update_candidate_profiles.js");
                     db.doQuery("update realtime_contribs a join koch_orgs b on donor_name = org_name set koch_tier = tier where koch_tier is null")
-                      .then(function() { return db.doQuery("delete from koch_contribs where source = 'realtime'"); })
-                      .then(function() { return db.doQuery("insert into koch_contribs select * from realtime_contribs"); })
-                      .then(function() { return db.doQuery("update candidates set 2014contrib = 0, since2000contrib = 0"); })
-                      .then(function() { return db.doQuery("update candidates join (select sum(amount) as total, sum(if(cycle = 2014, amount,0)) as current,  votesmartid from koch_contribs where for_against = 'f' group by votesmartid) b using(votesmartid) set 2014contrib = current, since2000contrib = total"); })
+                      .then(function() { return update.updateCandidates(); })
                       .then(function() {
                         cleanupDeferred.resolve();   
                       })
